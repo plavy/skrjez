11d62f68189c218b39a98e2acab017550ff31b1e
Tin Plavec <tinplavec@gmail.com>
2022-05-02 18:30:20 +0200

    zad4 done

---
+++ lab2/zadatak4.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+
+while(<>) {
+  if(/[0-9]/) {
+    /(.*);(.*);(.*);([0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}):[0-9]{2}.*;([0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2})/;
+    $jmbag = $1;
+    $prezime = $2;
+    $ime = $3;
+    $pocetak_dan = $4;
+    $pocetak_sat = $5;
+    $kraj_dan = $6;
+    $kraj_vrijeme = $7;
+    $kraj_vrijeme =~ /([0-9]{2})/;
+    $kraj_sat = $1;
+    if ($pocetak_dan ne $kraj_dan || $pocetak_sat ne $kraj_sat) {
+      print "$jmbag $prezime $ime - PROBLEM: $pocetak_dan $pocetak_sat:00 --> $kraj_dan $kraj_vrijeme", "\n";
+    }
+  }
+}
