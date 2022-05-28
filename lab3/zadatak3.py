a4acb4d6ccb3dac31829306b127f51da57be7b1a
Tin Plavec <tinplavec@gmail.com>
2022-05-28 12:13:29 +0200

    zad3 done

--- lab3/zadatak3.py
+++ lab3/zadatak3.py
@@ -19,12 +19,26 @@ if __name__ == "__main__":
         d[units[0]] = {}
         d[units[0]]['prezime'] = units[1]
         d[units[0]]['ime'] = units[2]
+    lab_brojevi = set()
     for file in files:
-        lab_broj = file.split('_')[1]
+        lab_broj = 'L' + str(int(file.split('_')[1]))
+        lab_brojevi.add(lab_broj)
         with open(dir + '/' + file) as f:
             lines = f.readlines()
             for line in lines:
                 jmbag = line.strip().split()[0]
-                bod = line.strip().split()[1]
+                bod = float(line.strip().split()[1])
+                if jmbag in d and lab_broj in d[jmbag]:
+                    print(f'WARNING: Student {jmbag} has conflicts for lab{lab_broj}')
                 d[jmbag][lab_broj] = bod
-    print(d)
+    print(f'{"JMBAG":14s} {"Prezime, Ime":26s}', end='')
+    for el in sorted(lab_brojevi):
+        print(f'{el:6s}', end='')
+    print()
+    for jmbag in d:
+        prez_ime = f'{d[jmbag]["prezime"]}, {d[jmbag]["ime"]}'
+        print(f'{jmbag:14s} {prez_ime:26s}', end='')
+        for el in sorted(lab_brojevi):
+            bod = f'{d[jmbag][el]:<6.1f}' if el in d[jmbag] else f'{"-":6s}'
+            print(bod, end='')
+        print()
